cd4945eaa1c101955de90b3cd66e7102197200f3
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-02-08 21:13:53 -0500

    changes to the back end

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,18 @@
 const User = require("../models/user.model");
+const jwt = require('jsonwebtoken');
 
 module.exports.createUser = (req, res) => {
     User.create(req.body)
-    .then(user => res.json(user))
-    .catch(err => res.status(400).json(err));
+    .then(user => {
+        const userToken = jwt.sign({
+            id: user._id
+        }, process.env.process.env.FIRST_SECRET_KEY);
+
+        res.cookie("usertoken", userToken, {
+            httpOnly: true
+        })
+        .json({ msg: "success!", user: user});
+    })
 }
 
 module.exports.getUser = (req, res) => {
@@ -31,4 +40,31 @@ module.exports.deleteUser = (req,res) => {
     User.deleteOne({_id: req.params.id})
     .then(deleteduser => res.json(deleteduser))
     .catch(err => res.status(400).json(err));
+}
+
+module.exports.login = async(req, res) => {
+    const user = await User.findOne({ email: req.body.email});
+    if(user === null) {
+        return res.sendStatus(400);
+    }
+
+    const correctPassword = await bcrypt.compair(req.body.password, user.password);
+
+    if(!correctPassword){
+        return res.sendStatus(400);
+    }
+
+    const userToken = jwt.sign({
+        id: user._id
+    }, process.env.FIRST_SECRET_KEY);
+
+    res.cookie("usertoken", userToken, {
+        httpOnly: true
+    })
+    .json({ msg: "cookie obtained!"});
+}
+
+module.exports.logout = (req, res) => {
+    res.clearCookie('usertoken');
+    res.sendStatus(200);
 }
