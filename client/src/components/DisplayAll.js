e349b2ca68491887b341a7a30037044a998e7db2
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-02-10 23:15:58 -0500

    finished display body

--- client/src/components/DisplayAll.js
+++ client/src/components/DisplayAll.js
@@ -1,7 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
 const DisplayAll = (props) => {
+    const [blogList, setBlogList] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/blog')
+        .then(res => {
+            console.log(res.data);
+            setBlogList(res.data);
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
+
     return (
-        <div>
-            <h1>Beans</h1>
+        <div className="display-container">
+            <div className="row">
+                <div className="col-1">
+
+                </div>
+                <div className="col-2">
+                    {
+                        blogList && blogList.map((blog, index) => (
+                            <div key={index} className={'blog-container'}>
+                                <h2>{blog.creator}</h2>
+                                <h1><Link to={`/blog/${blog._id}`}>{blog.title}</Link></h1>
+                                <p>{blog.body}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="col-3">
+
+                </div>
+            </div>
         </div>
     )
 }
